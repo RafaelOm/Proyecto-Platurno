0cae2dd25c9981a8b5f3b4b488c4621446f6b2fa
javiermorenoperez <62284555+javiermorenoperez@users.noreply.github.com>
2021-04-24 19:13:21 +0200

    Encuesta EJB cambios

--- ProyectoPlaturno-ejb/src/main/java/es/uma/platurno/ejb/EncuestaEJB.java
+++ ProyectoPlaturno-ejb/src/main/java/es/uma/platurno/ejb/EncuestaEJB.java
@@ -1,11 +1,18 @@
 package es.uma.platurno.ejb;
 
+import java.util.List;
+
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import es.uma.platurno.ejb.exceptions.CuentaInactivaException;
+import es.uma.platurno.ejb.exceptions.CuentaInexistenceException;
+import es.uma.platurno.ejb.exceptions.PasswordErroneaException;
+import es.uma.platurno.ejb.exceptions.PlaturnoException;
 import es.uma.platurno.jpa.Encuesta;
+import es.uma.platurno.jpa.Usuario;
 
 @Local
 @Stateless
@@ -14,18 +21,25 @@ public class EncuestaEJB implements EncuestaInterfaceEJB {
 	@PersistenceContext(unitName = "ProyectoPlaturno.GrupoF")
 	private EntityManager em;
 	
+	
+	private Autenticacion auth;
+	
+  
+        
 	public EncuestaEJB()
     {
     }
 
-	public EncuestaEJB(java.sql.Date fechaEnvio, String texto) {
-		crearEncuesta(fechaEnvio, texto);
+	public EncuestaEJB(Usuario u, java.sql.Date fechaEnvio, String texto) throws PlaturnoException, CuentaInactivaException, CuentaInexistenceException, PasswordErroneaException {
+		crearEncuesta(u, fechaEnvio, texto);
 	}
 	
-	public void crearEncuesta(java.sql.Date fechaEnvio, String texto) {
-		 Encuesta enc = new Encuesta();
-		 enc.setFecha_de_Envio(fechaEnvio);
-		 enc.setTexto(texto);
-		 em.persist(enc);
+	public void crearEncuesta(Usuario u, java.sql.Date fechaEnvio, String texto) throws PlaturnoException, CuentaInactivaException, CuentaInexistenceException, PasswordErroneaException {
+		auth=new Autenticacion();
+        auth.compruebaLogin(u);
+		Encuesta enc = new Encuesta();
+		enc.setFecha_de_Envio(fechaEnvio);
+		enc.setTexto(texto);
+		em.persist(enc);
 	}
 }
