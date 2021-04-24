9bed2f7bfa6c779949705b8087cd49a4a47243f3
javiermorenoperez <62284555+javiermorenoperez@users.noreply.github.com>
2021-04-24 20:54:29 +0200

    Add files via upload

--- ProyectoPlaturno-ejb/src/test/java/es/uma/platurno/tests/ListadoAlumnosTest.java
+++ ProyectoPlaturno-ejb/src/test/java/es/uma/platurno/tests/ListadoAlumnosTest.java
@@ -14,18 +14,18 @@ import javax.naming.Context;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-import es.uma.informatica.sii.anotaciones.Requisitos;
-
 public class ListadoAlumnosTest {
 	
 	private static final Logger LOG = Logger.getLogger(AutenticacionEjbTest.class.getCanonicalName());
-
+	private static final String GLASSFISH_CONFIGI_FILE_PROPERTY = "org.glassfish.ejb.embedded.glassfish.configuration.file";
+	private static final String CONFIG_FILE = "src/test/resources/META-INF/domain.xml";
 	private static final String USUARIOEJB = "java:global/classes/UsuarioEjb!es.uma.platurno.ejb.UsuarioEjbInterfaz";
 	private static final String AUTENTICACION = "java:global/classes/Autenticacion!es.uma.platurno.ejb.AutenticacionInterfaz";
 	private static final String LISTADOALUM = "java:global/classes/ListadoAlumnos!es.uma.platurno.ejb.ListadoAlumnosInterface";
 	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "Trazabilidad";
 	
-	
+	private static EJBContainer ejbContainer;
+	private static Context ctx;
 
 	private UsuarioEjbInterfaz usuarioEjb;
 	private ListadoAlumnosInterface listadoEjb;
@@ -33,19 +33,24 @@ public class ListadoAlumnosTest {
 	
 
 	
-
+	@BeforeClass
+	public static void setUpClass() {
+		Properties properties = new Properties();
+		properties.setProperty(GLASSFISH_CONFIGI_FILE_PROPERTY, CONFIG_FILE);
+		ejbContainer = EJBContainer.createEJBContainer(properties);
+		ctx = ejbContainer.getContext();
+	}
 
 	@Before
 	public void setup() throws NamingException, javax.naming.NamingException {
 		//asignaturaEjb = (AsignaturasEjb) ctx.lookup(ASIGNATURAEJB);
-		usuarioEjb = (UsuarioEjbInterfaz) TestSuite.ctx.lookup(USUARIOEJB);
-		auth = (AutenticacionInterfaz) TestSuite.ctx.lookup(AUTENTICACION);
-		listadoEjb = (ListadoAlumnosInterface) TestSuite.ctx.lookup(LISTADOALUM);
+		usuarioEjb = (UsuarioEjbInterfaz) ctx.lookup(USUARIOEJB);
+		auth = (AutenticacionInterfaz) ctx.lookup(AUTENTICACION);
+		listadoEjb = (ListadoAlumnosInterface) ctx.lookup(LISTADOALUM);
 		
 		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
 	}
-	@Requisitos({"RF-08"})
-	@Ignore
+	
 	@Test
 	public void testCompruebaListado(){
 		
@@ -62,8 +67,7 @@ public class ListadoAlumnosTest {
 		
 		
 	}
-	@Requisitos({"RF-08"})
-	@Ignore
+	
 	@Test
 	public void testCompruebaFiltros(){
 		
@@ -82,6 +86,11 @@ public class ListadoAlumnosTest {
 
 	}
 		
-
+	@AfterClass
+	public static void tearDownClass() {
+		if (ejbContainer != null) {
+			ejbContainer.close();
+		}
+	}
 
 }
